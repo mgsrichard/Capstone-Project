c69de53208e329608c3e4a03268e0f1ed153120a
Lucien Roberts <lucienrobertsiv@gmail.com>
2022-04-23 13:25:30 -0400

    initial app commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
-with open(filepath, ) as file:
+with open('rf_model_pkl', 'rb') as file:
     model = pickle.load(file)
 
 app = Flask(__name__)
@@ -20,12 +20,14 @@ def home():
 def graphs():
     return render_template('graphs.html')
 
-@app.route("/predictor")
+@app.route("/predictor", methods=["GET","POST"])
 def predictor():
     if request.method == "POST":
         yearsofexperience = request.form["yearsofexperience"]
-
-        pred =  model.predict(np.array([]))
+        yearsatcompany = request.form["yearsatcompany"]
+        region = request.form["region"]
+        pred =  model.predict(np.array([[int(yearsofexperience), int(yearsatcompany), int(region)]]))
+        return render_template('predictor.html', pred=str(pred))
     return render_template('predictor.html')
 
 
