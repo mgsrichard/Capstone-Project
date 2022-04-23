b7ef7c334a7e6a289bd4492da13d12cee20481d4
Lucien Roberts <lucienrobertsiv@gmail.com>
2022-04-23 11:26:06 -0400

    model update

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+with open(filepath, ) as file:
+    model = pickle.load(file)
 
 app = Flask(__name__)
 
@@ -16,7 +22,12 @@ def graphs():
 
 @app.route("/predictor")
 def predictor():
+    if request.method == "POST":
+        yearsofexperience = request.form["yearsofexperience"]
+
+        pred =  model.predict(np.array([]))
     return render_template('predictor.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
